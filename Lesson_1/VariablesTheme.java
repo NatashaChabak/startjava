1b88d337f8b768118d2c36372f2e5542356873b2
NatashaChabak <nc1chabak@gmail.com>
2023-02-23 16:17:26 +0300

    Замечания к первому задания исправлены. Добавлено задание по if-els

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -1,19 +1,23 @@
 public class VariablesTheme {
 
     public static void main(String[] args) {
-
         System.out.println("\n1. Вывод значений переменных на консоль");
         byte processorNumber = 1;
         short memoryLimit = 6242;
         int memorySize = 16127;
-        System.out.println("Число процессоров " + processorNumber + "\nДоступная физическая память, МБ " + memoryLimit + "\nПолный объем физической памяти, МБ " + memorySize);
+        System.out.println("Число процессоров " + processorNumber + 
+                          "\nДоступная физическая память, МБ " + memoryLimit + 
+                          "\nПолный объем физической памяти, МБ " + memorySize);
         long virtualMemory = 21_036l;
         float procMemoryUsage = 85.814f;
         double procFreeMemory = 14.185_206_312_987_259d;
-        System.out.println("Виртуальная память: Макс. размер,МБ " + virtualMemory + "\nВиртуальная память: Используется, % " + procMemoryUsage + "\nВиртуальная память: Доступнo, % " + procFreeMemory);
+        System.out.println("Виртуальная память: Макс. размер,МБ " + virtualMemory + 
+                           "\nВиртуальная память: Используется, % " + procMemoryUsage + 
+                           "\nВиртуальная память: Доступнo, % " + procFreeMemory);
         char domainName = 'A';
         boolean availableHyperV = true;
-        System.out.println("Домен " + domainName + "\nРасширения режима мониторинга виртуальной машины " + availableHyperV);
+        System.out.println("Домен " + domainName + 
+                           "\nРасширения режима мониторинга виртуальной машины " + availableHyperV);
 
         System.out.println("\n2. Расчет стоимости товара со скидкой");
         int penPrice = 100;
@@ -21,8 +25,10 @@ public class VariablesTheme {
         int discount  = 11;
         int totalAmount = penPrice + bookPrice;
         System.out.println("Общая стоимость товаров без скидки, руб. " + totalAmount);
-        System.out.println("Сумма скидки, руб. " + (totalAmount * discount/100));
-        System.out.println("Общая стоимость товаров со скидкой, руб. " + (totalAmount - totalAmount * discount/100));
+        int discountAmount = totalAmount * discount/100;
+        System.out.println("Сумма скидки, руб. " + discountAmount);
+        int finalAmount = totalAmount - discountAmount;
+        System.out.println("Общая стоимость товаров со скидкой, руб. " + finalAmount);
 
         System.out.println("\n3. Вывод слова JAVA");
         System.out.println("   J    a  v     v  a    ");
@@ -31,44 +37,69 @@ public class VariablesTheme {
         System.out.println(" JJ  a     a  V  a     a");
 
         System.out.println("\n4. Вывод min и max значений целых числовых типов");
-        byte myByte = 127;
-        short myShort = 32_767;
-        int myInt = 2_147_483_647;
-        long myLong = Long.MAX_VALUE;
-        float myFloat = Float.MAX_VALUE;
-        double myDouble = Double.MAX_VALUE;
-        System.out.println("byte. Первоначальное значение " + myByte + "; значение после инкремента " + (++myByte) + "; значение после декремента " + (--myByte-1));
-        System.out.println("short. Первоначальное значение " + myShort + "; значение после инкремента " + (++myShort) + "; значение после декремента " + (--myShort-1));
-        System.out.println("int. Первоначальное значение " + myInt + "; значение после инкремента " + (++myInt) + "; значение после декремента " + (--myInt-1));
-        System.out.println("long. Первоначальное значение " + myLong + "; значение после инкремента " + (++myLong) + "; значение после декремента " + (--myLong-1));
-        System.out.println("float. Первоначальное значение " + myFloat + "; значение после инкремента " + (++myFloat) + "; значение после декремента " + (--myFloat-1));
-        System.out.println("double. Первоначальное значение " + myDouble + "; значение после инкремента " + (++myDouble) + "; значение после декремента " + (--myDouble-1));
+        byte exampleByte = 127;
+        short exampleShort = 32_767;
+        int exampleInt = 2_147_483_647;
+        long exampleLong = Long.MAX_VALUE;
+        float exampleFloat = Float.MAX_VALUE;
+        double exampleDouble = Double.MAX_VALUE;
+        System.out.println("byte. Первоначальное значение " + exampleByte + 
+                           "; значение после инкремента " + (++exampleByte) + 
+                           "; значение после декремента " + (--exampleByte-1));
+        System.out.println("short. Первоначальное значение " + exampleShort + 
+                           "; значение после инкремента " + (++exampleShort) + 
+                           "; значение после декремента " + (--exampleShort-1));
+        System.out.println("int. Первоначальное значение " + exampleInt + 
+                           "; значение после инкремента " + (++exampleInt) + 
+                           "; значение после декремента " + (--exampleInt-1));
+        System.out.println("long. Первоначальное значение " + exampleLong + 
+                           "; значение после инкремента " + (++exampleLong) + 
+                           "; значение после декремента " + (--exampleLong-1));
+        System.out.println("float. Первоначальное значение " + exampleFloat + 
+                           "; значение после инкремента " + (++exampleFloat) +
+                           "; значение после декремента " + (--exampleFloat-1));
+        System.out.println("double. Первоначальное значение " + exampleDouble + 
+                           "; значение после инкремента " + (++exampleDouble) + 
+                           "; значение после декремента " + (--exampleDouble-1));
 
         System.out.println("\n5. Перестановка значений переменных");
         int a = 2;
         int b = 5;
-        System.out.println("Перестановка с помощью третьей переменной. Исходные значения: a =  " + a + "; b = " + b);
+        System.out.println("Перестановка с помощью третьей переменной.");
+        System.out.println("Исходные значения: a =  " + a + "; b = " + b);
         int c = a;
         a = b;
         b = c;
         System.out.println("Новые значения: a =  " + a + "; b = " + b);
-        System.out.println("\nПерестановка с помощью арифметических операций. Исходные значения: a =  " + a + "; b = " + b );
-        a = a + b;
+        System.out.println("Перестановка с помощью арифметических операций.");
+        System.out.println("Исходные значения: a =  " + a + "; b = " + b);
+        a += b;
         b = a - b;
-        a = a - b;
+        a -= b;
         System.out.println("Новые значения: a =  " + a + "; b = " + b);
-        System.out.println("\nПерестановка с помощью побитовой операции. Исходные значения: a =  " + a + "; b = " + b );
-        a = a ^ b; 
-        b = a ^ b;
-        a = a ^ b;
+        System.out.println("Перестановка с помощью побитовой операции.");
+        System.out.println("Исходные значения: a =  " + a + "; b = " + b);
+        a ^= b; 
+        b ^= a;
+        a ^= b;
         System.out.println("Новые значения: a =  " + a + "; b = " + b);
 
         System.out.println("\n6. Вывод символов и их кодов");
-        char[] listChar = new char[]{'#', '&', '@', '^', '_'};
-        for (char m : listChar) {
-            int codeSymv = (int)m;
-            System.out.println("symbol: " + m + " code: " + codeSymv);
-        }
+        char charValue = '#';
+        int symboleCode = (int) charValue;
+        System.out.println(charValue + " " + symboleCode);
+        charValue = '&';
+        symboleCode = (int) charValue;
+        System.out.println(charValue + " " + symboleCode);
+        charValue = '@';
+        symboleCode = (int) charValue;
+        System.out.println(charValue + " " + symboleCode);
+        charValue = '^';
+        symboleCode = (int) charValue;
+        System.out.println(charValue + " " + symboleCode);
+        charValue = '_';
+        symboleCode = (int) charValue;
+        System.out.println(charValue + " " + symboleCode);
 
         System.out.println("\n7. Вывод в консоль ASCII-арт Дюка");
         char s1 = '/';
@@ -85,33 +116,18 @@ public class VariablesTheme {
         System.out.println("\n8. Вывод количества сотен, десятков и единиц числа");
         int initNumber = 123;
         int hundreds = initNumber / 100;
-        int dozens = (initNumber - hundreds * 100) / 10;
-        int units = (initNumber - hundreds * 100 - dozens * 10);
-        System.out.println("Число " + initNumber + " содержит:\n" + hundreds + " сотен\n" + dozens + " десятков\n" + units + " единиц");
+        int dozens = initNumber / 10 % 10;
+        int units = initNumber % 10;
+        System.out.println("Число " + initNumber + " содержит:\n" + hundreds + 
+                           " сотен\n" + dozens + " десятков\n" + units + " единиц");
         System.out.println("Сумма его цифр =  " + (hundreds + dozens + units));
         System.out.println("Произведение  =  " + (hundreds * dozens * units));
 
         System.out.println("\n9. Вывод времени");
-        int sek = 86399;
-        int hours = sek / 3600;
-        int min = (sek - hours *3600) / 60;
-        sek = (sek - hours *3600 - min * 60);
-        System.out.println(hours + ":" + min + ":" + sek);
-        //System.out.println("minutes " + min);
-        //System.out.println(a - a % 100);
-/*
-        int b = 5;
-        int c = a ^ b;
-        char[] listChar = new char[]{35, 38, 64, 94, 95};
-        char myChar = 35;
-        //System.out.println(myChar);S
-        for (char m : listChar) {
-            System.out.println((int)m);
-        }
-        /*
-        System.out.println(Integer.toBinaryString(a));
-        System.out.println(Integer.toBinaryString(b));
-        System.out.println(Integer.toBinaryString(c));
-        */
+        int seconds = 86399;
+        int hours = seconds / 3600;
+        int minutes = (seconds - hours *3600) / 60;
+        seconds = (seconds - hours *3600 - minutes * 60);
+        System.out.println(hours + ":" + minutes + ":" + seconds);
     }
 }
