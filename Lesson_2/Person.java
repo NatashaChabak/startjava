dbcffeeda3473452b9f0eac797ed1446c7decfda
NatashaChabak <nc1chabak@gmail.com>
2023-03-06 13:03:42 +0300

    Урок 2, ДЗ 1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    boolean isMale = true;
+    String name = "David";
+    int height = 182;
+    int weight = 82;
+    int age = 82;
+
+    void walk() {
+        System.out.println("walking");
+    }
+
+    void sit() {
+        System.out.println("sitting");
+    }
+
+    void run() {
+        System.out.println("running");
+    }
+
+    void speak() {
+        System.out.println("speaking");
+    }
+
+    void learnJava() {
+        System.out.println("learning Java");
+     }
+}
