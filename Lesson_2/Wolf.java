bbd082287b45c151030ccd9f1e6ded225ce07e65
NatashaChabak <nc1chabak@gmail.com>
2023-03-06 21:21:57 +0300

    Задание 2 к уроку 2

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+    public boolean isMale;
+    public String name;
+    public int weight;
+    public int age;
+    public String color;
+
+    public void walk() {
+        System.out.println("walking");
+    }
+
+    public void sit() {
+        System.out.println("sitting");
+    }
+
+    public void run() {
+        System.out.println("running");
+    }
+
+    public void howl() {
+        System.out.println("howling");
+    }
+
+    public void hunt() {
+        System.out.println("hunting");
+    }
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,12 @@
+public class WolfTest {
+
+    public static void main(String[] args) {
+        Wolf wolf = new Wolf();
+        wolf.name = "Bart";
+        wolf.age = 7;
+        wolf.color = "red";
+        wolf.weight = 12;
+        wolf.isMale = true;
+        System.out.println(wolf.name + " is a " + wolf.color + " wolf "+ wolf.age + " years old");
+    }
+}
