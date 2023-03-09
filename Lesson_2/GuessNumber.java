45d1eaf634ba49976fb9cfb86f10e0ab01c12c88
NatashaChabak <nc1chabak@gmail.com>
2023-03-09 21:14:59 +0300

    Задание Загадай число к уроку 2

---
+++ Lesson_2/GuessNumber.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+
+    private Player player1;
+    private Player player2;
+
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void Game() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int min = 1;
+        int max = 100;
+        int hiddenNum = random.nextInt(max) + min;
+        String name1 = player1.getName();
+        String name2 = player2.getName();
+        String name;
+        int num;
+        int counter = 0;
+        do {
+            name = counter % 2 == 0 ? name1 : name2;
+            System.out.print(name + ", введите любое число от " + min + " до " + max + ": ");
+            num = scanner.nextInt();
+            if (num < hiddenNum) {
+                System.out.println("Число " + num + " меньше того, что загадал компьютер");
+                min = num;
+            } else if (num > hiddenNum) {
+                System.out.println("Число " + num + " больше того, что загадал компьютер");
+                max = num;
+            }
+            counter++;
+        } while (hiddenNum != num);
+        System.out.println(name + ", Вы угадали! Число = " + hiddenNum);
+    }
+}
---
+++ Lesson_2/GuessNumberTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя первого игрока: ");
+        String name1 = scanner.nextLine();
+        Player player1 = new Player(name1);
+        System.out.print("Введите имя второго игрока: ");
+        String name2 = scanner.nextLine();
+        Player player2 = new Player(name2);
+        String answer;
+        do {
+            GuessNumber guessNumber = new GuessNumber(player1, player2);
+            guessNumber.Game();
+            System.out.print("Хотите продолжить вычисления? [yes/no] ");
+            answer = scanner.nextLine();
+        } while (!answer.equals("no"));
+    }
+}
