ad5d6e4f3ad2421aa1fe01ca0d1618c6a845b28a
NatashaChabak <nc1chabak@gmail.com>
2023-03-21 16:46:28 +0300

    ДЗ к уроку 3, калькулятор, попытка 2

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -2,22 +2,21 @@ package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
 
-    public static double calculate(int a, int b, String sign) {
-        double result = 0d;
-        switch (sign) {
-            case "+":
-                return (a + b);
-            case "-":
-                return (a - b);
-            case "*":
-                return Math.multiplyExact(a, b);
-            case "/":
-                return (double) a / (double) b;
-            case "^":
-                return Math.pow(a, b);
-            default:
-                System.out.println("Cannot recognize sign " + sign);
-                return result;
-        }
+    public static double calculate(String input) {
+        String[] inputArr = input.split(" ");
+        int a = Integer.parseInt(inputArr[0]);
+        String sign = inputArr[1];
+        int b = Integer.parseInt(inputArr[2]);
+        double result = switch (sign) {
+            case "+" -> (a + b);
+            case "-" -> (a - b);
+            case "*" -> (Math.multiplyExact(a, b));
+            case "/" -> ((double) a / (double) b);
+            case "^" -> (Math.pow(a, b));
+            default ->  throw new IllegalArgumentException(
+                        "Cannot recognize sign");
+        };
+        System.out.print(a + " " + sign + " " + b + " = ");
+        return result;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -9,7 +9,7 @@ public class CalculatorTest {
         String answer = "yes";
         while (true) {
             if (!answer.equals("yes")) {
-                System.out.print("Хотите продолжить вычисления? [yes/no] ");
+                System.out.print("\nХотите продолжить вычисления? [yes/no] ");
                 answer = scanner.nextLine();
                 if (answer.equals("no")) {
                     return;
@@ -19,12 +19,8 @@ public class CalculatorTest {
             }
             System.out.print("Введите математическое выражение: ");
             String input = scanner.nextLine();
-            String[] inputArr = input.split(" ");
-            int a = Integer.parseInt(inputArr[0]);
-            String sign = inputArr[1];
-            int b = Integer.parseInt(inputArr[2]);
-            double result = Calculator.calculate(a, b, sign);
-            System.out.println(a + " " + sign + " " + b + " = " + (result % 1 == 0 ? String.format("%,.0f", result) : String.format("%,.3f", result) ));
+            double result = Calculator.calculate(input);
+            System.out.print(result % 1 == 0 ? String.format("%,.0f", result) : String.format("%,.3f", result));
             answer = "";
           }
     }
